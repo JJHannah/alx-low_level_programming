24cb5dc79a4fe4c8de5974d817549506b8d75443
JJHannah <zemjefferyzem@gmail.com>
2022-09-09 08:06:40 -0700

    3-Alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+/**
+ * main- Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char lower = 'a';
+
+char upper = 'A';
+
+while (lower <= 'z') /*print lowercases a-z*/
+{
+putchar(lower);
+lower++;
+}
+while (upper <= 'Z') /*print uppercase A-Z*/
+{
+putchar(upper);
+upper++;
+}
+putchar('\n');
+
+return (0);
+}
