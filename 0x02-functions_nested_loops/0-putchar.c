0983269c5bd94231561c9880235025b52383df06
JJHannah <zemjefferyzem@gmail.com>
2022-09-14 10:49:32 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,10 +7,9 @@
 
 int main(void)
 {
-char message[] ="_putchar";
-int i;
-for (int i = 0;i <=8; i++)
-_putchar(str[i]);
+char c ="_putchar";
+for (c = 0;c <=8; c++)
+_putchar(str[c]);
 _putchar('\n');
 return (0);
 }
