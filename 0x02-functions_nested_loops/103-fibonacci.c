07b98e2b654f2f1244a81c06c878d714f5dabac3
JJHannah <zemjefferyzem@gmail.com>
2022-09-14 18:44:28 -0700

    15-. Evev

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *main - Entry poin
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+unsigned long int j, k, next, sum;
+
+j = 1;
+k = 2;
+sum = 0;
+
+for (i = 1; i <= 33; ++i)
+{
+if (j < 4000000 && (j % 2) == 0)
+{
+sum = sum + j;
+}
+next = j + k;
+j = k;
+k = next;
+}
+printf("%lu\n", sum);
+return (0);
+}
