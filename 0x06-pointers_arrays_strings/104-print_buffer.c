d7ea2d12895856d14e9077a1078ea17e55bdb33f
JJHannah <zemjefferyzem@gmail.com>
2022-09-21 15:51:11 -0700

    12-Noise is a buffer

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_buffer - prints a s bytes of a buffer
+ *inputted number of bytes from a buffer.
+ *Prints 10 bytes per line.
+ *Starts with the position of the first byte in hexadecimal (8 chars)
+ *starting with `0`.
+ *Each line shows the hexadecimal content (2 chars) of the buffer
+ *2 bytes at a time, separated by a space.
+ *Each line shows the content of the buffer
+ * Each line ends with a new line `\n`
+ *@b: number of bytes
+ *@size: size of the byte
+ * Return: void
+ */
+
+void print_buffer(char *b, int size)
+{
+int x = 0, y;
+if (size < 0)
+{
+printf('\n');
+return;
+}
+while (x < size)
+{
+if (x % 10 == 0)
+printf("%08x: ", x);
+for (y = x; y < x + 9; y += 2)
+{
+if ((y < size) && ((y + 1) < size))
+printf("%02x%02x: ", b[y], b[y + 1]);
+else
+{
+while (++y <= x + 10)
+printf(" ");
+printf(" ");
+}
+}
+for (y = x; y < x + 9 && y < size; y++)
+{
+if (b[y] >= 32 && b[y] <= 126)
+printf("%c", b[y]);
+else
+printf(".");
+}
+printf('\n');
+x += 10;
+}
+}
