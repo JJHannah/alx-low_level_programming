047ae85ee85baf4df503a3dee2f777b57a07bb5b
JJHannah <zemjefferyzem@gmail.com>
2022-09-21 14:22:26 -0700

    11-It is the addition

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -14,11 +14,9 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 int i, j, k, x, y, z;
 
-for (i = 0; n1[i]; i++);
-
-for (j = 0; n2[j]; j++);
-
-if (i > size_r || j > size_r);
+for (i = 0; n1[i]; i++)
+for (j = 0; n2[j]; j++)
+if (i > size_r || j > size_r)
 return (0);
 
 y = 0;
@@ -46,4 +44,4 @@ r[k] = r[x];
 r[x] = m;
 }
 return (r);
-}
+i}
