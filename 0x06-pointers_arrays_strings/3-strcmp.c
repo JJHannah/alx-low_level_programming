2ceb4b569e08936e26a6390002b847771d78c23f
JJHannah <zemjefferyzem@gmail.com>
2022-09-21 09:46:42 -0700

    3-strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_strcmp - compares two strings
+ *@s1: first string
+ *@s2: second string
+ *Return: outputs of the difference of s1 and s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+int j = 0;
+while (*(s1 + j) && *(s2 + j) && (*(s1 + j) == *(s2 + j)))
+j++;
+
+return (*(s1 + j) - *(s2 + j));
