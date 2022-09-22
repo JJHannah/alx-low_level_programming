fa0ec715858999f417d5b435492936d1d3c26e0a
JJHannah <zemjefferyzem@gmail.com>
2022-09-22 13:21:55 -0700

    11- It is the addition

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -12,36 +12,48 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int i, j, k, x, y, z;
+int i = 0, j = 0,   k,  x,  y,    z, add =0;
+int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
 
-for (i = 0; n1[i]; i++)
-for (j = 0; n2[j]; j++)
-if (i > size_r || j > size_r)
+while (*(n1 + i) != '\0')
+i++;
+
+while (*(n2 + j) != '\0')
+j++;
+if (i >= j)
+x = i;
+else
+x = j;
+if (size_r <= x + 1)
 return (0);
-
+r[x + 1] = '\0';
+i--, j--, size_r--;
+y = *(n1 + i) - 48, z = *(n2 + j) - 48;
+while (x >= 0)
+{
+k = y + z + add;
+if (k >= 10)
+add = k / 10;
+else
+add = 0;
+if (k > 0)
+*(r + x) = (k % 10) + 48;
+else
+*(r + x) = '0';
+if (i > 0)
+i--, y = *(n1 + i) - 48;
+else
 y = 0;
-for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
-{
-y = z;
-if (i >= 0)
-z += n1[i] - '0';
-if (j >= 0)
-z += n2[j] - '0';
-if (i < 0 && j < 0 && n == 0)
-{
-break;
+if (j > 0)
+j--, z = *(n2 + j) - 48;
+else
+z = 0;
+x--, size_r--;
 }
-y = z / 10;
-r[k] = z % 10 + '0';
-}
-r[k] = '\0';
-if (i >= 0 || j >= 0 || y)
-return (0);
-for (k -= 1, x = 0; x < k; k--, x++)
-{
-y = r[k];
-r[k] = r[x];
-r[x] = m;
-}
+if (*(r) == '0')
+return (r + 1);
+else
 return (r);
-i}
+}
+
+
