ce275a9a445f24c9d74555240d5307e50f45aa54
JJHannah <zemjefferyzem@gmail.com>
2022-09-22 05:48:49 -0700

    10- A dream does not become reality through magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+/**
+ *main -Entry point
+ *-You are not allowed to use a
+ *-You are not allowed to modify p
+ *-only one statement
+ *-you are not allowed to code anything else than this line of code
+ *Return: 0
+ */
+
 int main(void)
 {
 int n;
@@ -8,14 +17,7 @@ int *p;
 
 a[2] = 1024;
 p = &n;
-/*
- *write your line of code here...
- *Remember:
- *-You are not allowed to use a
- *-You are not allowed to modify p
- *-only one statement
- *- you are not allowed to code anything else than this line of code
- */
+
 *(p + 5) = 98;
 /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
