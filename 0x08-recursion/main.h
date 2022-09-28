9851dd86f103cb47e2696cdab6cb8bde43b87df7
JJHannah <zemjefferyzem@gmail.com>
2022-09-28 12:44:47 -0700

    Initial commit

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,5 +11,7 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int _sqrt(int n, int i);
 int is_prime(int n, int start);
-	
+int is_palindrome(char *s);
+void check(char *s , int start, int end, int *flag);
+
 #endif /* MAIN_H */
