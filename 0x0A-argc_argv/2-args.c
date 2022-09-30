7eb05cb03db16221332033ed751a8c1c85d4ac47
JJHannah <zemjefferyzem@gmail.com>
2022-09-30 03:43:50 -0700

    Initial commit

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main - print product of argument numbers
+ *@argc: argument counter
+ *@argv: numbers to multiply
+ *Return: 0 on success
+ */
+ 
+int main(int argc, char *argv[])
+{
+int x = 0;
+
+while (x < argc)
+printf("%s\n", argv[x++]);
+
+return (0);
+}
