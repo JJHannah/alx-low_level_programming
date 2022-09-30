8749303555c2876972d30131ad96182979e18d47
JJHannah <zemjefferyzem@gmail.com>
2022-09-30 03:28:42 -0700

    0-It is  what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,8 +8,9 @@
  *Return: 0
  */
 
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
-_putchar("%s\n", argv[0])
+if (argc > 0)
+printf("%s\n", argv[0]);
 return (0);
 }
