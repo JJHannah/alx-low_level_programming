e4e6d590b8e20d4f04dfad5e757df18ccec78e5e
JJHannah <zemjefferyzem@gmail.com>
2022-10-11 10:41:35 -0700

    initial commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -23,5 +23,7 @@ dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
 
 #endif
