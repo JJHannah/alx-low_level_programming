ca1254f79f0f8d459b8652c499b58d54f010cbe9
JJHannah <zemjefferyzem@gmail.com>
2022-10-24 11:02:14 -0700

    initial commit

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,20 +11,31 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-list_t *new;
-unsigned int len = 0;
-
-while (str[len])
-len++;
+list_t *new, *temp;
+size_t nchar;
 
 new = malloc(sizeof(list_t));
-if (!new)
+if (new == NULL)
 return (NULL);
 
 new->str = strdup(str);
-new->len = len;
-new->next = (*head);
-(*head) = new;
+for (nchar = 0; str[nchar]; nchar++)
+{
+;
+}
+new->len = nchar;
+new->next = NULL;
+temp = *head;
 
+if (temp == NULL)
+{
+*head = new;
+}
+else
+{
+while (temp->next != NULL)
+temp = temp->next;
+temp->next = new;
+}
 return (*head);
 }
