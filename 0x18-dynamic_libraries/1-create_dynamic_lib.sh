a1f3caca1595fa2d86d79e5fe23e492837fa148b
JJHannah <zemjefferyzem@gmail.com>
2022-12-13 13:52:15 -0800

    commit3

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,2 +1,4 @@
-#!/bin/bashi
-export LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH
+#!/bin/bash
+gcc -Wall -Wextra -Werror -pedantic -c -fPIC *.c
+gcc -shared -o liball.so *.o
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
